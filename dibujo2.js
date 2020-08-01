07f8ae60186e84d71521bbe6a40976cdcd47f257
lpzsds <69083739+lpzsds@users.noreply.github.com>
2020-08-01 12:35:13 -0500

    Add files via upload

--- dibujo2.js
+++ dibujo2.js
@@ -1,7 +1,7 @@
 // Llamando al canvas y definiendo el tipo de dibujo (2d)
 var p = document.getElementById("papel");
 var areadedibujo = p.getContext("2d");
-
+var color;
 
 
 
@@ -23,4 +23,21 @@ function circulo(color, x, y, rGrados)
   areadedibujo.arc(x, y, rGrados, angulo_partida, angulo_final);
   areadedibujo.stroke();
   areadedibujo.closePath();
+}
+
+linea("blue", 0, 1, 3, 1)
+//Funcion que hace una linea - draw a line
+function linea(color, x0, y0, xf, yf)
+{
+//Obtener pendiente de la recta
+  var m = (yf-y0)/(xf-x0);
+//interseccion con el eje y
+  var b = y0 - m*x0;
+  var longitud_linea = Math.sqrt( (xf-x0)*(xf-x0) + (yf-y0)*(yf-y0)  );
+  var distancia_entrecirculos
+
+  for (var distancia_entrecirculos = 0; distancia_entrecirculos < array.length; i++) {
+    array[i]
+  }
+
 }
